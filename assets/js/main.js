3ae57500772995a141ad90250b7b92ee7d4b1325
s7arlord <rathilvasani@gmail.com>
2018-02-25 21:45:41 +0530

    checking conuter and navbar v1

--- assets/js/main.js
+++ assets/js/main.js
@@ -77,7 +77,7 @@ const second = 1000,
       hour = minute * 60,
       day = hour * 24;
 
-let countDown = new Date('March 24, 2018 09:00:00').getTime(),
+let countDown = new Date('Sep 30, 2018 00:00:00').getTime(),
     x = setInterval(function() {
 
       let now = new Date().getTime(),
