9a24b9e0acdc909841d5b6cded5ddd3976236c25
s7arlord <rathilvasani@gmail.com>
2018-02-26 19:16:40 +0530

    adding images to logo and settin up the timeline

--- assets/js/main.js
+++ assets/js/main.js
@@ -95,3 +95,10 @@ let countDown = new Date('March 24, 2018 09:00:00').getTime(),
       //}
 
     }, second)
+
+
+
+		$(".timeline-wrapper .timeline-content-item > span").on("mouseenter mouseleave", function(e){
+		  $(".timeline-wrapper .timeline-content-item.active").removeClass("active");
+		  $(this).parent().addClass("active");
+		});
